45a1309a6f0b199d6cce0b3295d4cb2ad3e7d2c1
BigBlueHat <byoung@bigbluehat.com>
2013-11-04 16:50:29 -0500

    added simple request debugging
    Very handy to see what the request includes.
    
    Add `?debug=true` (or `1`) to any request.

--- shows/html.js
+++ shows/html.js
@@ -1,10 +1,13 @@
 function(doc, req) {
-  var mustache = require("lib/mustache");
+  var mustache = require("lib/mustache"),
+      debug = ('debug' in req.query
+          ? req.query.debug === 'true' || req.query.debug === "1"
+          : false);
   return mustache.to_html(this.templates.html, {
         page: doc,
         site: {
           base_url: '/' + req.path.slice(0,3).join('/') + '/_rewrite/'
         },
-        req: JSON.stringify(req.path.slice(0,3))
+        req: (debug ? JSON.stringify(req, null, '\t') : '')
       });
 }

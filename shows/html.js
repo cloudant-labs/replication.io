4623acb264e079921a6b43db9bdb7b1be24db40a
BigBlueHat <byoung@bigbluehat.com>
2013-11-04 17:13:47 -0500

    added `active` state to navigation
    Easier to know where one is.

--- shows/html.js
+++ shows/html.js
@@ -2,12 +2,22 @@ function(doc, req) {
   var mustache = require("lib/mustache"),
       debug = ('debug' in req.query
           ? req.query.debug === 'true' || req.query.debug === "1"
-          : false);
-  return mustache.to_html(this.templates.html, {
+          : false),
+      mustache_view = {
         page: doc,
+        active: doc._id,
+        is_active: function() {
+          return function (param) {
+            if (param == this.active) {
+              return 'active';
+            }
+          }
+        },
         site: {
           base_url: '/' + req.path.slice(0,3).join('/') + '/_rewrite/'
         },
         req: (debug ? JSON.stringify(req, null, '\t') : '')
-      });
+      };
+
+  return mustache.to_html(this.templates.html, mustache_view);
 }
